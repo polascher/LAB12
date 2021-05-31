a6ce5c013500dba31e478d8e5fc4550dd2b7c3f2
polascher <polascher@gmail.com>
2021-05-31 18:07:55 +0700

    LAB12

---
+++ LAB12/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LAB12
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите любой символ: ");
+            string x = Console.ReadLine();
+            int y = 0;
+            bool result = int.TryParse(x, out y);
+            if (result == true)
+                Console.WriteLine("Введенный символ - цифра");
+            else
+                Console.WriteLine("Введенный символ - не цифра!");
+        } 
+    }
+}
